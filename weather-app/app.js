9b7d47543a702f99630722296aca01f281c60df5
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-03 19:11:44 -0500

    Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const request = require('postman-request');
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API}&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API}&query=37.8267,-122.4233&units=f`;
 
-request(url, (err, res, body) => {
-  const data = JSON.parse(body);
-  console.log(data.current);
+request({ url, json: true }, (err, res, body) => {
+  console.log(
+    `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside. It feels like ${body.current.feelslike} degress out.`
+  );
 });
