8b3f6dcb5d3bc43d903ed50299bfbf0908e82811
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-03 19:44:18 -0500

    HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const request = require('postman-request');
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API}&query=37.8267,-122.4233&units=f`;
+const weatherstackUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API}&query=37.8267,-122.4233&units=f`;
 
-request({ url, json: true }, (err, res, body) => {
+request({ url: weatherstackUrl, json: true }, (err, res, body) => {
   console.log(
     `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside. It feels like ${body.current.feelslike} degress out.`
   );
+});
+
+const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API}&limit=1`;
+
+request({ url: mapboxUrl, json: true }, (err, res, body) => {
+  const latitude = body.features[0].center[1];
+  const longitude = body.features[0].center[0];
+  console.log(latitude, longitude);
 });
